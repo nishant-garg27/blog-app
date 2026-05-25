bb8e23ee641c23dc60f9fdfa70e25ea2fc1f8244
Demo <nishantgarg270906@gmail.com>
2026-05-25 23:58:26 +0530

    feat/Add-Blog>

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const path = require('path')
 const userRoute = require('./routes/user')
+
+const blogRoute = require('./routes/blog')
+
 const mongoose = require('mongoose')
 const app = express();
 const PORT = 8000
@@ -9,6 +12,7 @@ const { checkCookieAuthentication } = require('./middleware/authentication')
 mongoose.connect('mongodb://localhost:27017/blogify').then((e) => {
     console.log('mongoDB connected');
 })
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
@@ -22,5 +26,6 @@ app.get('/', (req,res) => {
 })
 
 app.use('/user', userRoute)
+app.use('/blog', blogRoute)
 
 app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`)) 
